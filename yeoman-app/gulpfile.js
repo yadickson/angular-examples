4d47fc77d05a987a7cfeca11f9d03ca390b7bac9
Yadickson Soto <yadickson.soto@basedos.cl>
2017-10-11 14:49:07 -0300

    Add view files

--- yeoman-app/gulpfile.js
+++ yeoman-app/gulpfile.js
@@ -54,7 +54,7 @@ gulp.task('lint:test', () => {
 });
 
 gulp.task('html', ['styles', 'scripts'], () => {
-  return gulp.src('app/*.html')
+  return gulp.src('app/**/*.html')
     .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
     .pipe($.if(/\.js$/, $.uglify({compress: {drop_console: true}})))
     .pipe($.if(/\.css$/, $.cssnano({safe: true, autoprefixer: false})))
